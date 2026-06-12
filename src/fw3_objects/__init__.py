95461ee3e3a116ecd623070a4312fd28d79889e0
iamdefinitelyahuman <ben@hauser.id>
2026-06-12 22:11:18 +0000

    feat: expose top-level objects

---
+++ src/fw3_objects/__init__.py
@@ -0,0 +1,12 @@
+from .account import Account, Accounts
+from .chain import Chain
+from .contract import Contract
+from .transaction import Transaction
+
+__all__ = [
+    "Account",
+    "Accounts",
+    "Chain",
+    "Contract",
+    "Transaction",
+]
